fd4eb6d14298fbfd1de5af6135f0979df013ca23
underwater2 <shaian2@naver.com>
2022-11-25 10:02:30 +0900

    #6.3 Recap: 주석 추가

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
-  // 컴포넌트의 첫번째 렌더링에만 해당 함수가 실행되도록 함
+  // useEffect : 코드를 언제 실행할지 결정할 수 있음
   useEffect(() => {
     console.log("I run only once.");
-  }, []);
+  }, []); // 컴포넌트의 첫번째 렌더링에만 해당 함수가 실행되도록 함 (한 번)
   useEffect(() => {
     if (keyword !== "" && keyword.length > 5) {
       console.log("SEARCH FOR", keyword);
@@ -17,7 +17,7 @@ function App() {
   }, [keyword]); // keyword가 변화할 때마다 실행된다
   useEffect(() => {
     console.log("I run when keyword & counter change");
-  }, [keyword, counter]);
+  }, [keyword, counter]); // 둘 중 하나가 변화할 때마다 실행된다
   return (
     <div>
       <input
