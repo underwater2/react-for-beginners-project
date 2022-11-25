c061cd0440bcf2d59ab096d3dcecd5bfd9feae36
underwater2 <shaian2@naver.com>
2022-11-25 09:38:14 +0900

    #6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  console.log("i run all the time");
+  // 컴포넌트의 첫번째 렌더링에만 해당 함수가 실행되도록 함
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
