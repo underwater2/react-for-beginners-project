f12ec7f85419137ff845b0a33772b7c3d728fe69
underwater2 <shaian2@naver.com>
2022-11-30 17:04:37 +0900

    #7.5 React Router: 중간과정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
