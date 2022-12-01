9c2b7077c3d19207723cd7e7554bc32713adf6cb
underwater2 <shaian2@naver.com>
2022-12-01 11:00:46 +0900

    #7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
