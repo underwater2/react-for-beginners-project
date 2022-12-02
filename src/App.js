a7cf5fbae2b43753c8498668a869f2c35b3f9eaa
underwater2 <shaian2@naver.com>
2022-12-02 09:41:15 +0900

    #7.7 Publishing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <Routes>
         <Route path="/abot-us" element={<h1>Hello</h1>}></Route>
         <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
       </Routes>
     </Router>
   );
